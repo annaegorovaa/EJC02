d64a51275d21815b3c8873a8546b31a12bad22a0
annaegorovaa <anju.egorova@gmail.com>
2017-10-26 19:00:38 +0300

    lesson8: TaskException2 final changes

--- src/test/java/com/epam/l8/TaskException2Test.java
+++ src/test/java/com/epam/l8/TaskException2Test.java
@@ -1,6 +1,5 @@
 package com.epam.l8;
 
-import com.epam.l7.TaskException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,62 +13,18 @@ public class TaskException2Test {
     }
 
     @Test
-    public void TestCheckInputIs1() {
+    public void TestCheckInputIsCorrect() {
         Assert.assertEquals("January", exception2.checkInput("1"));
-    }
-
-    @Test
-    public void TestCheckInputIs2() {
         Assert.assertEquals("February", exception2.checkInput("2"));
-    }
-
-    @Test
-    public void TestCheckInputIs3() {
         Assert.assertEquals("March", exception2.checkInput("3"));
-    }
-
-    @Test
-    public void TestCheckInputIs4() {
         Assert.assertEquals("April", exception2.checkInput("4"));
-    }
-
-    @Test
-    public void TestCheckInputIs5() {
         Assert.assertEquals("May", exception2.checkInput("5"));
-    }
-
-    @Test
-    public void TestCheckInputIs6() {
         Assert.assertEquals("June", exception2.checkInput("6"));
-    }
-
-    @Test
-    public void TestCheckInputIs7() {
         Assert.assertEquals("July", exception2.checkInput("7"));
-    }
-
-    @Test
-    public void TestCheckInputIs8() {
         Assert.assertEquals("August", exception2.checkInput("8"));
-    }
-
-    @Test
-    public void TestCheckInputIs9() {
         Assert.assertEquals("September", exception2.checkInput("9"));
-    }
-
-    @Test
-    public void TestCheckInputIs10() {
         Assert.assertEquals("October", exception2.checkInput("10"));
-    }
-
-    @Test
-    public void TestCheckInputIs11() {
         Assert.assertEquals("November", exception2.checkInput("11"));
-    }
-
-    @Test
-    public void TestCheckInputIs12() {
         Assert.assertEquals("December", exception2.checkInput("12"));
     }
 
